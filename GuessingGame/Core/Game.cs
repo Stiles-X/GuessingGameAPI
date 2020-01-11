1483aafff8ecba868350301b05fb569da0614e02
Wai Yan Myint Mo <dragonkey100@gmail.com>
2020-01-11 16:23:41 +0630

    refactored 'game.correct' error handling

--- GuessingGame/Core/Game.cs
+++ GuessingGame/Core/Game.cs
@@ -41,14 +41,11 @@ namespace GuessingGame.Core
             get { return _Correct; }
             set
             {
-                if (_Max.HasValue & _Min.HasValue) // Check if min or max have been set, if so
-                {
-                    if ((value > _Max) | (value < _Min)) // Check if value is {read the code}
-                        throw new ArgumentOutOfRangeException("value", "Correct can not be outside of max and min range"); // value out of range
-                    _Correct = value; // Then set
-                }
-                else
+                if (!(_Max.HasValue | _Min.HasValue)) // Check if min or max have been set, if so
                     throw new ArgumentNullException("Max, Min", "Max or Min value has not been set");
+                if ((value > _Max) | (value < _Min)) // Check if value is {read the code}
+                    throw new ArgumentOutOfRangeException("value", "Correct can not be outside of max and min range"); // value out of range
+                _Correct = value; // Then set
             }
         }
 
@@ -74,9 +71,11 @@ namespace GuessingGame.Core
             UsedGuesses = _UsedGuesses + 1;
             return result;
         }
-        // Allowed Guesses (to be use with guess)
+
+        // TO BE USED WITH GUESSES
+        // Allowed Guesses
         public int? AllowedGuesses { get; set; }
-        // Used Guesses (to be use with allowed guesses)
+        // Used Guesses
         private int _UsedGuesses { get; set; }
         public int UsedGuesses
         {
@@ -89,6 +88,12 @@ namespace GuessingGame.Core
                     throw new ArgumentOutOfRangeException("UsedGuesses", "Number of used guesses can not be more than allowed guesses");
                 _UsedGuesses = value;
             }
+        }
+
+        // Random (generate a valid int guess from max min range)
+        public static void Random()
+        {
+
         }
     }
 }
