e95faa41a74253a5b52d1402e0a2cbc1683ae3f7
Wai Yan Myint Mo <dragonkey100@gmail.com>
2020-01-11 17:22:18 +0630

    correctrandom

--- GuessingGame/Core/API.cs
+++ GuessingGame/Core/API.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace GuessingGame.Core
 {
-    class SinglePlayer
+    class API
     {
-        public SinglePlayer()
+        public API()
         {
             _CoreGame = new CoreGame();
         }
@@ -21,5 +21,22 @@ namespace GuessingGame.Core
         // MIN - GET / SET
         public int? GetMin() { return _CoreGame.Min; }
         public void SetMin(int Min) { _CoreGame.Min = Min; }
+
+        // Correct - GET / SET
+        public int? GetCorrect() { return _CoreGame.Correct; }
+        private bool _IsCorrectSet { get; set; }
+        public void SetCorrect(int Correct)
+        {
+            if (_IsCorrectSet)  // Checking so that you can only set Correct one time
+                throw new ArgumentException("You have already set the correct answer");
+            _IsCorrectSet = true;
+            _CoreGame.Correct = Correct;
+        }
+        public void SetCorrectRandom()  // So that you can play single player
+        {
+            SetCorrect(_CoreGame.Random);
+        }
+
+
     }
 }
