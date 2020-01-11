4da7a3567ee7ebb71a3dd37aaa4940f51b244dba
Wai Yan Myint Mo <dragonkey100@gmail.com>
2020-01-11 17:11:34 +0630

    min max

--- GuessingGame/Core/API.cs
+++ GuessingGame/Core/API.cs
@@ -14,5 +14,12 @@ namespace GuessingGame.Core
         // Game (storing a game instance)
         private CoreGame _CoreGame { get; set; }
 
+        // MAX - GET / SET
+        public int? GetMax() { return _CoreGame.Max; }
+        public void SetMax(int Max) { _CoreGame.Max = Max; }
+
+        // MIN - GET / SET
+        public int? GetMin() { return _CoreGame.Min; }
+        public void SetMin(int Min) { _CoreGame.Min = Min; }
     }
 }
