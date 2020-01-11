bd90367a8d256a2d40bb8f8f22d2382145ad9387
Wai Yan Myint Mo <dragonkey100@gmail.com>
2020-01-11 14:53:18 +0630

    test start

---
+++ GuessingGame/TestStart/TestStart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessingGame
+{
+    class TestStart
+    {
+        public static void TestMain()
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
