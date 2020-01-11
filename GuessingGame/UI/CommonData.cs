143c821c27929863e622557607cb0aeb9a95fa3d
Wai Yan Myint Mo <dragonkey100@gmail.com>
2020-01-12 00:51:16 +0630

    fancy ascii display

--- GuessingGame/UI/CommonData.cs
+++ GuessingGame/UI/CommonData.cs
@@ -7,7 +7,25 @@ namespace GuessingGame.UI
     class CommonData
     {
         public static readonly string help = "Usage: help, --help, -h, version, --version, -v";
-        public static readonly string version = "Rapid Development Stage";
+        public static readonly string version = "0.0.0";
         public static readonly string not_found = "Command not found. Usage: help, --help, -h, version, --version, -v";
+        public static string ascii_logo { get { return GetAsciiLogo(); } }
+        public static string GetAsciiLogo(string text = "")
+        {
+            return
+                @"
+
+   _____                     _             
+  / ____|                   (_)            
+ | |  __ _   _  ___  ___ ___ _ _ __   __ _ 
+ | | |_ | | | |/ _ \/ __/ __| | '_ \ / _` |
+ | |__| | |_| |  __/\__ \__ \ | | | | (_| |
+  \_____|\__,_|\___||___/___/_|_| |_|\__, |
+  / ____|                             __/ |
+ | |  __  __ _ _ __ ___   ___        |___/ 
+ | | |_ |/ _` | '_ ` _ \ / _ \             
+ | |__| | (_| | | | | | |  __/             
+  \_____|\__,_|_| |_| |_|\___|" + $"{text}    \n";
+        }
     }
 }
--- GuessingGame/UI/UI.cs
+++ GuessingGame/UI/UI.cs
@@ -8,7 +8,14 @@ namespace GuessingGame.UI
     {
         public static void UIMain(string[] args)
         {
+            Console.Clear();
             Args.HandleArgs(args);
+            MainMenu();
+            Console.Read();
+        }
+        public static void MainMenu()
+        {
+            Console.WriteLine(CommonData.GetAsciiLogo("v"+CommonData.version));
         }
     }
 }
