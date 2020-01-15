391b2b9ab91b2c1bc82d8900e66cb78762362efa
Wai Yan Myint Mo <dragonkey100@gmail.com>
2020-01-15 17:03:56 +0630

    added interface for model

--- API/API.cs
+++ API/API.cs
@@ -6,5 +6,59 @@ namespace API
 {
     class API : APIInterface
     {
+        public void GetAllowedGuesses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCorrect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetUsedGuesses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Guess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAllowedGuesses(int AllowedGuesses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCorrect(int Correct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMax(int MaxV)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMin(int Min)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetUsedGuesses(int UsedGuesses)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- API/LogicInterface.cs
+++ API/LogicInterface.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace API
 {
-    class Model : ModelInterface
+    interface LogicInterface
     {
     }
 }
